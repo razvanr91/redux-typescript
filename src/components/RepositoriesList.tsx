8acc68e39dc45aa68ee353e99393b1f00cc1ef2c
razvanr91 <razvan.roman91@outlook.com>
2022-02-07 01:50:58 +0000

    Update RepositoriesList.tsx

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { actionCreators } from "../state";
 
 const RepositoriesList: React.FC = () => {
+	const dispatch = useDispatch();
+	const [term, setTerm] = useState("");
+
+	const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setTerm(e.target.value);
+	};
+
+	const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		dispatch(actionCreators.searchRepositories(term));
+	};
+
 	return (
 		<div>
-			<form>
-				<input />
+			<form onSubmit={onSubmit}>
+				<input value={term} onChange={onChange} />
 				<button>Search</button>
 			</form>
 		</div>
