1670410bb7f4384ff4c622429d6df78f980d5d42
razvanr91 <razvan.roman91@outlook.com>
2022-02-08 14:00:56 +0000

    Update repositoriesReducer.ts

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,31 +1,12 @@
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+
 const initialState: RepositoriesState = { loading: false, error: null, data: [] };
 
 interface RepositoriesState {
 	loading: boolean;
 	error: string | null;
 	data: string[];
-}
-
-interface SearchRepositoriesAction {
-	type: ActionType.SEARCH_REPOSITORIES;
-}
-
-interface SearchRepositoriesSuccessAction {
-	type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
-	payload: string[];
-}
-
-interface SearchRepositoriesErrorAction {
-	type: ActionType.SEARCH_REPOSITORIES_ERROR;
-	payload: string;
-}
-
-type Action = SearchRepositoriesAction | SearchRepositoriesSuccessAction | SearchRepositoriesErrorAction;
-
-enum ActionType {
-	SEARCH_REPOSITORIES = "search_repositories",
-	SEARCH_REPOSITORIES_SUCCESS = "search_repositories_success",
-	SEARCH_REPOSITORIES_ERROR = "search_repositories_error",
 }
 
 const repositoriesReducer = (state: RepositoriesState = initialState, action: Action): RepositoriesState => {
