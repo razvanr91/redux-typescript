c2b078864787c9ebb41175640f88e6ab565be385
razvanr91 <razvan.roman91@outlook.com>
2022-02-09 12:08:04 +0000

    Update index.ts

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { ActionType } from "../action-types";
 import { Action } from "../actions";
 
-const searchRepositories = (term: string) => {
+export const searchRepositories = (term: string) => {
 	return async (dispatch: any) => {
 		dispatch({
 			type: ActionType.SEARCH_REPOSITORIES,
